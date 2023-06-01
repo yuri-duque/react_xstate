82828d0a8455b2e6c53605db83ab1e7242b28684
yuriduque-ciandt <yurithielmann83@gmail.com>
2023-06-01 09:29:03 -0300

    creating my first machine

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useMachine } from '@xstate/react';
+import { myMachine } from './machines/myFirstMachine';
 
 function App() {
+  const [state, send] = useMachine(myMachine);
+
+  const setHouver = () => {
+    send('MOUSEOVER')
+  }
+
+  const setNotHouver = () => {
+    send('MOUSEOUT')
+  }
+
   return (
     <div className="App">
-      Learn React
+      {JSON.stringify(state.value)}
+      <button onMouseOver={setHouver} onMouseOut={setNotHouver}>Hover</button>
     </div>
   );
 }
