83eaefd21e94865b8d8557ffddb8e59bfcded798
yuriduque-ciandt <yurithielmann83@gmail.com>
2023-06-02 11:50:09 -0300

    project with storybook 7 and library

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,17 @@
-const config = {
+import type { StorybookConfig } from "@storybook/react-webpack5";
+const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
     "@storybook/addon-interactions",
     "storybook-xstate-addon/preset",
   ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
   docs: {
     autodocs: "tag",
   },
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,18 @@
-export const parameters = {
-  xstate: {
-    height: "500px",
+import type { Preview } from "@storybook/react";
+
+const preview: Preview = {
+  parameters: {
+    xstate: {
+      height: "500px",
+    },
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
   },
 };
+
+export default preview;
